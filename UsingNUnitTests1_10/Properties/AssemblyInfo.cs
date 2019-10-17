c80367ef360cc6ad0c810b6dfdd6883ef0481e4b
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-17 20:30:09 +0700

    1_10

---
+++ UsingNUnitTests1_10/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UsingNUnitTests1_10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UsingNUnitTests1_10")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29620fa5-4bed-4b6c-9553-7010b113fbf1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UsingNUnitTests1_10/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NUnit.Framework;
+using UsingNUitinVisualStudio1_10.Fundamentals;
+
+namespace UsingNUnitTests1_10
+{
+
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrage
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+            Assert.That(result, Is.True);
+        }
+        [Test]
+        public void CanBeCancelledBy_SameUserCancellingTheReservation_Return()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancellingReservation_RetureFalse()
+        {
+            var reservation = new Reservation { MadeBy = new User() };
+
+            var result = reservation.CanBeCancelledBy(new User());
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
+        {
+
+        }
+
+
+        [Test]
+        public void CanBeCancelledBy_SameUseCancelling_ReturnTrue()
+        {
+
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancelling_ReturnFalse()
+        {
+
+        }
+
+    }
+}
